ce1d1855f0b99dc3f261fbb63b9bc863b212a549
shivam kumar singh <89032612+shivam-0109@users.noreply.github.com>
2022-07-30 10:30:20 +0530

    Create compare-two-number.cpp

---
+++ compare-two-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two number to compare :";
+    cin>>a>>b;
+    
+    if(a>b){
+        cout<<"Greater number is :"<<a<<endl;
+    }else {
+        cout<<"Greater number is :"<<b<<endl;
+    }
+    return 0;
+}
