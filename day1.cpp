a2e325720949b691eeb9dda4d77ac9334c08c875
shivam kumar singh <89032612+shivam-0109@users.noreply.github.com>
2022-07-30 10:16:36 +0530

    Create day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world"<<endl;
+    //this is a single line comment 
+    /* this 
+    is 
+    a 
+    multiline comment
+    */
+}
