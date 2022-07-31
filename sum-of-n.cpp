fa09dd35f7cdb6b14ee0a8290b9a196e304d5d2a
shivam kumar singh <89032612+shivam-0109@users.noreply.github.com>
2022-07-31 19:12:02 +0530

    Create sum-of-n.cpp

---
+++ sum-of-n.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    int i=1;
+    int sum=0;
+    while(i<=a){
+       sum+=i;
+        i++;
+        
+    }
+    cout<<sum;
+    return 0;
+}
