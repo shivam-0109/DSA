24175f4f71b91f88462ff21b16ff68e5063be974
shivam kumar singh <89032612+shivam-0109@users.noreply.github.com>
2022-07-31 19:08:09 +0530

    Create print-number-upto-n.cpp

---
+++ print-number-upto-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    int i=1;
+    while(i<=a){
+        cout<<i<<endl;
+        i++;
+        
+    }
+    return 0;
+}
