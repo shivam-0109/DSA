fee729f4c2d3ccc4cb996564b63936064c310979
shivam kumar singh <89032612+shivam-0109@users.noreply.github.com>
2022-08-17 07:04:32 +0530

    Create reverse_a_string.cpp
    Write function that take a string then reverse it.

---
+++ reverse_a_string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   string a;
+   cin>>a;
+   reverse(a.begin(), a.end());
+   cout<<a;
+
+    return 0;
+}
