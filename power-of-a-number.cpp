d2cfba0ff4ebd1c792bad9ce988a665305994611
shivam kumar singh <89032612+shivam-0109@users.noreply.github.com>
2022-08-17 06:55:54 +0530

    Create power-of-a-number.cpp
    Write a C++ program that take a number from user then output the power of this number.

---
+++ power-of-a-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+   int number,power;
+   cout<<"Enter the number : ";
+   cin>>number;
+   
+   cout<<"Enter the power : ";
+   cin>>power;
+   
+cout<<pow(number,power)<<endl;
+    
+    return 0;
+}
